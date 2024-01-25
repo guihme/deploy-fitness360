9d34f8ae35129dd24bbf924b9b426e4dab4178e9
guihme <guilhermecruz@discente.ufg.br>
2024-01-24 22:34:21 -0300

    add custom css swagger

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,10 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+  });
 
   await app.listen(process.env.PORT);
 
